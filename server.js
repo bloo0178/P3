257a986b27cb3dcadce7eb971cfcb805837227a1
bloo0178 <bloo0178@gmail.com>
2018-09-15 22:01:21 -0400

    1

--- server.js
+++ server.js
@@ -2,41 +2,53 @@
 
 var express = require('express');
 var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
 require('dotenv').config();
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+var bodyParser = require('body-parser');
+console.log(process.env.MONGO_URI);
+console.log('test');
 
-var cors = require('cors');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
 
-var app = express();
+  var cors = require('cors');
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+  var app = express();
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+  // Basic Configuration 
+  var port = process.env.PORT || 3000;
 
-app.use(cors());
+  /** this project needs a db !! **/
+  // mongoose.connect(process.env.MONGOLAB_URI);
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+  app.use(cors());
 
-app.use('/public', express.static(process.cwd() + '/public'));
+  /** this project needs to parse POST bodies **/
+  // you should mount the body-parser here
+  app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+  app.use('/public', express.static(process.cwd() + '/public'));
+
+  app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+
+  // your first API endpoint... 
+  app.get("/api/hello", function (req, res) {
+    res.json({ greeting: 'hello API' });
+  });
+
+
+  app.listen(port, function () {
+    console.log('Node.js listening ....');
+  });
+
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
-
-app.listen(port, function () {
-  console.log('Node.js listening ....');
-});
 
 //npm install ... modules (add dotenv, nodemon)
 //set root to cd\github\p3
