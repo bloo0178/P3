c54af2674df2fbac7d48d0d784cfd139d704a992
bloo0178 <bloo0178@gmail.com>
2018-09-16 21:48:38 -0400

    0916-2148

--- server.js
+++ server.js
@@ -9,92 +9,115 @@ var bodyParser = require('body-parser');
 console.log(process.env.MONGO_URI);
 console.log('test');
 
-//var db = mongoose.connection;
-//db.on('error', console.error.bind(console, 'connection error:'));
-//db.once('open', function () {
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  /*Wipe the database on start so index/ shortURL code starts from 0 
+  for each use*/
+  db.dropDatabase();
 
-  var cors = require('cors');
-
-  
-  var app = express();
   // Basic Configuration 
+  var cors = require('cors');
+  var app = express();
   var port = process.env.PORT || 3000;
-  
-
-  /** this project needs a db !! **/
-  // mongoose.connect(process.env.MONGOLAB_URI);
-
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cors());
-
-  /** this project needs to parse POST bodies **/
-  // you should mount the body-parser here
-  app.use(bodyParser.urlencoded({extended: false}));
-
+  const dns = require('dns');
+  const url = require('url');
   app.use('/public', express.static(process.cwd() + '/public'));
 
   app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-  /*       ---LOGIC---
-  -Pull the URL from the field
-  -Test if the URL is valid
-  -Show the json at api/shorturl/new
-  -Store the URL in the database via json
-  -Allow for an ad-hoc URL at api/shorturl/[x]
-  --Need to search database for x
-  --If doesn't exist, "error: no short URL for given input"
-  */
-
-
-
   var Schema = mongoose.Schema;
+
+  var index = 0;
+
   var shorturlSchema = new Schema({
-    originalurl : String,
-    //index : Number,
-   // shorturl : String
+    originalurl: String,
+    index: Number, //this also servers as the shortURL
+    //shorturl : Number
   });
 
   var ShortURL = mongoose.model('ShortURL', shorturlSchema);
 
+  /*This function uses Node's DNS functionality to validate
+  a URL. This will be chained within the POST function below.*/
+  let validateURL = (URL, callback) => {
+    let testURL = url.parse(URL);
+    testURL = testURL.hostname;
+    dns.lookup(testURL, (err, addresses) => {
+      if (addresses == undefined) {
+        callback(err);
+      }
+      else {
+        callback();
+      }
+    })
+  }
 
+  //pulls the URL from the field within the body
+  app.post('/api/shorturl/new', function (req, res) {
+    index += 1;
+    //check for valid URL 
+    let inputURL = req.body.url;
+    let regex = /^http(s?):\/\/.*/i; //have to accomodate nothing preceding www. as well
+    validateURL(inputURL, function (err) {
+      if (err || !regex.test(inputURL)) {
+        res.json({ callback: 'error with the URL' })
 
-    //pulls the URL from the field within the body
-    app.post('/api/shorturl/new', function(req, res, next) {
-      let newURL = new ShortURL({
-        originalurl : req.body.url, 
-      });
-      newURL.save(function(err) {
-        if (err) {
-          res.send(err);
-        }
-        console.log("DB successfully updated...");
-        //return done(null, newURL);
-      });
-      next();
-    }, function( req, res) {
-      res.json({test2: req.body.url});
-    }
-    );
-
-
-
-  // your first API endpoint... 
-  app.get("/api/hello", function (req, res) {
-    res.json({ greeting: 'hello API' });
+      } else {
+        let newURL = new ShortURL({
+          originalurl: req.body.url,
+          index: index
+        });
+        newURL.save(function (err) {
+          if (err) {
+            res.send(err);
+          }
+          console.log("DB successfully updated...");
+          res.json({ original_url: req.body.url, short_url: newURL.index });
+        });
+      }
+    })
   });
 
-  
+  //Chained in the app.get function below. Declared here for readability. 
+  let findURLByIndex = function (URLindex, callback) {
+    ShortURL.find({ index: URLindex }, (err, record) => {
+      if (err) {
+        callback(err, null);
+      } else if (record == null) {
+        callback(null,null);
+      }
+      else {
+        console.log("Record found..." + record[0]);
+        callback(null, record[0]);
+      }
+    });
+  };
 
+  app.get('/api/shorturl/:index', function (req, res) {
+    let searchIndex = req.params.index;
+    findURLByIndex(searchIndex, function (err, index) {
+      if (err) {
+        console.log(err);
+      } else if (index == null) {
+        res.send('ShortURL does not exist.');
+      } else 
+      {
+      res.redirect(index.originalurl);
+      }
+    });
+  });
 
   app.listen(port, function () {
     console.log('Node.js listening ....');
   });
+});
 
-//});
-
-
-
+/* Local Run Instructions - will need to set up project on Glitch */
 //npm install ... modules (add dotenv, nodemon)
 //set root to cd\github\p3
 //use nodemon server.js to start live monitoring
